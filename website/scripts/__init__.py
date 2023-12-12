b7a15af4273634da5bf71548e2311b9d682fedd7
KaieScarpato <116307680+KaieScarpato@users.noreply.github.com>
2023-12-12 16:01:28 -0500

    Add files via upload

--- website/scripts/__init__.py
+++ website/scripts/__init__.py
@@ -1,8 +1,9 @@
+import openpyxl
+import random
+from ..models import Beer,User
+from .. import db
+
 def init_database():
-    import openpyxl
-    import random
-    from ..models import Beer
-    from .. import db
     dataframe = openpyxl.load_workbook("Beerco_kiosk_data.xlsx")
     dataframe1 = dataframe.active
     d = 0
@@ -18,13 +19,36 @@ def init_database():
                         srm=str(row[21].value),malt=str(row[22].value),hops=str(row[23].value),cals=str(row[25].value),
                         desc=str(row[26].value),special=sp,searchFreq=sf)
         db.session.add(new_beer)
-        db.session.commit()
         if d >= 3:
             sp = 0
             p = float(random.randint(10, 15))
         sf = random.randint(0, 1000)
         d += 1
+
+    admin = User(role = 1, name="Admin", password=hash("password", "salt"), salt="salt")
+    retailer = User(role = 2, name="Retailer", password=hash("password","hello"), salt="hello")
+    kiosk = User(role = 3, name="Kiosk", password=hash("password","tang"), salt="tang")
+    
+    db.session.add(admin)
+    db.session.add(retailer)
+    db.session.add(kiosk)
+    db.session.commit()
+    
     print('DB initialized!')
+
+def hash(string, salt):
+    pw = bytearray(string+salt, encoding='utf-8')
+    copy = bytearray()
+    for c in pw:
+        x = chr((c >> 1) + 65).encode('utf-8')
+        copy.extend(x)
+    return copy.decode('utf-8')    
+
+def comparePasswords(password, user):
+    hashedPassword =  hash(password, user.salt)
+    if hashedPassword == user.password:
+        return True
+    return False
 
 def findTotal(cart):
     total = 0.0
