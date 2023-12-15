24f550ad97bcf545fd5f46d0349f9a3ca45491ad
MSaylan <144388734+MSaylan@users.noreply.github.com>
2023-12-14 19:58:26 -0500

    Add files via upload

---
+++ automated test.py
@@ -0,0 +1,108 @@
+import pyautogui
+import time
+
+# Dummy Coordinates (Replace these with actual coordinates on your screen)
+search_bar_coordinates = (100, 100)
+search_results_coordinates = (200, 200)
+selected_product_coordinates = (300, 300)
+like_products_coordinates = (400, 400)
+specials_tab_coordinates = (500, 500)
+retailer_interface_coordinates = (600, 600)
+product_update_page_coordinates = (700, 700)
+database_update_interface_coordinates = (800, 800)
+download_database_button_coordinates = (900, 900)
+
+# Test Case 1: UC-1 Customer Searches for a Product
+def test_case_1():
+    # Steps
+    pyautogui.click(search_bar_coordinates)
+    pyautogui.write("Product Name")  
+    time.sleep(1)  # Add a delay for the search results to appear
+
+    # Verification
+    assert "Search Results" in pyautogui.screenshot(region=(100, 100, 800, 600)).tobytes()
+
+# Test Case 2: UC-2 Customer Views Specified Product Information
+def test_case_2():
+    # Steps
+    pyautogui.click(selected_product_coordinates)
+    time.sleep(1)  # Add a delay for the product information to load
+
+    # Verification
+    assert "Product Information" in pyautogui.screenshot(region=(300, 300, 800, 600)).tobytes()
+
+# Test Case 3: UC-3 Customer Views Like Products
+def test_case_3():
+    # Steps
+    pyautogui.click(selected_product_coordinates)
+    time.sleep(1)  # Add a delay for the product information to load
+    assert "Like Products" in pyautogui.screenshot(region=(300, 300, 800, 600)).tobytes()
+
+# Test Case 4: UC-5 Retailer Creates Specials Page
+def test_case_4():
+    # Steps
+    pyautogui.click(retailer_interface_coordinates)
+    time.sleep(1)  # Add a delay for the retailer interface to load
+    pyautogui.write("Product Name")  # 
+    pyautogui.click("Flag as Special")  # Click the button to flag as special
+    time.sleep(1)  # Add a delay for the system to update
+
+    # Verification
+    assert "Specials Page Updated" in pyautogui.screenshot(region=(600, 600, 800, 600)).tobytes()
+
+# Test Case 5: UC-6 Retailer Requests Search Frequency Report
+def test_case_5():
+    # Steps
+    pyautogui.click(retailer_interface_coordinates)
+    time.sleep(1)  # Add a delay for the retailer interface to load
+    pyautogui.click("Request Search Report")  # Click the button to request a search report
+    time.sleep(1)  # Add a delay for the system to generate the report
+
+    # Verification
+    assert "Search Report Generated" in pyautogui.screenshot(region=(600, 600, 800, 600)).tobytes()
+
+# Test Case 6: UC-7 Retailer Updates Product Price
+def test_case_6():
+    # Steps
+    pyautogui.click(retailer_interface_coordinates)
+    time.sleep(1)  # Add a delay for the retailer interface to load
+    pyautogui.click(product_update_page_coordinates)
+    pyautogui.write("New Price")  # 
+    pyautogui.click((800, 800))  # Replace with actual coordinates for input field
+    time.sleep(1)  # Add a delay for the update to take effect
+
+    # Verification
+    assert "Price Updated" in pyautogui.screenshot(region=(600, 600, 800, 600)).tobytes()
+
+# Test Case 7: UC-8 Administrator (Beerco) Updates Database
+def test_case_7():
+    # Steps
+    pyautogui.click(database_update_interface_coordinates)
+    pyautogui.click((800, 800))  # 
+    time.sleep(1)  # Add a delay for the update to take effect
+
+    # Verification
+    assert "Database Updated" in pyautogui.screenshot(region=(800, 800, 800, 600)).tobytes()
+
+# Test Case 8: UC-9 Kiosk Downloads Database to Local Storage
+def test_case_8():
+    # Steps
+    pyautogui.click(download_database_button_coordinates)
+    time.sleep(1)  # Add a delay for the download to complete
+
+    # Verification
+    assert "Database Downloaded" in pyautogui.screenshot(region=(900, 900, 800, 600)).tobytes()
+
+# Execute All Test Cases
+try:
+    test_case_1()
+    test_case_2()
+    test_case_3()
+    test_case_4()
+    test_case_5()
+    test_case_6()
+    test_case_7()
+    test_case_8()
+    print("All test cases passed!")
+except AssertionError as e:
+    print(f"Test case failed: {e}")
